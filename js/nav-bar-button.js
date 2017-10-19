535d10d0be74c028604b0462ed23710494ed26fa
Jakub Kozlik <kozlik.jakub@gmail.com>
2017-10-20 01:19:48 +0200

    done counters animation script in welcome section

--- js/nav-bar-button.js
+++ js/nav-bar-button.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('Nav-bar-button.js loaded');
+    console.log('nav-bar-button.js loaded');
 
     var navBar = document.querySelector('.nav-bar');
     var navBarButton = document.querySelector('.nav-bar-button');
-    console.log(navBarButton);
+    //console.log(navBarButton);
 
     navBarButton.addEventListener('click', function(){
         this.classList.toggle('active-nav-bar-button');
--- js/social_counters.js
+++ js/social_counters.js
@@ -1,24 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('social_counters.js loaded');
 
+    //ten skrypt obciaza troche procka mimo ze iteruje 0.111 zamiast 0.001
+
     var socialCounters = document.querySelectorAll('.social-counter span');
-    console.log(socialCounters);
+    //console.log(socialCounters);
+    var span0 = Number(socialCounters[0].textContent.replace(',','.'));
+    var span1 = Number(socialCounters[1].textContent.replace(',','.'));
+    var span2 = Number(socialCounters[2].textContent.replace(',','.'));
+    //console.log(span0, span1, span2);
 
+    //animacje counterow trwaja mniej wiecej tyle samo (ok. 5 sekund) niezaleznie od liczby w przykladowym projekcie wiec musimy wyliczyc stosunek potrzebnych iteracji o 0.111 do czasu trwania animacji
+    var span0IntervalDelay = 5000 / (span0 / 0.111);
+    var span1IntervalDelay = 5000 / (span1 / 0.111);
+    var span2IntervalDelay = 5000 / (span2 / 0.111);
 
-    // for(var i = 1; i < 20000; i++){
-    //     setTimeout(function(){
-    //         socialCounters[1].textContent = i;
-    //     }, 1000)
-    // }
+    var counter0 = 0;
+    var counter1 = 0;
+    var counter2 = 0;
 
+    var setIntervalId0 = setInterval(function(){
+        counter0+=0.111;
+        //wywalamy znaki bedace dalej niz 3 po przecinku i zastepujemy ulamkowa kropke przecinkiem
+        socialCounters[0].textContent = counter0.toString().substring(0, counter0.toString().indexOf('.') + 4).replace('.',',');
+        if(counter0 > span0){
+            clearInterval(setIntervalId0);
+            //wyliczenia nie sa celne co do 0.001 wiec pod koniec animacji i tak podstawiamy okreslona w htmlu wartosc
+            socialCounters[0].textContent = span0.toString().replace('.',',');
+        }
+    }, span0IntervalDelay);
 
-    //  var setIntervalId = setInterval(function(){
-    //      counter++;
-    //      socialCounters[1].textContent = counter;
-    //      if(counter === 20000){
-    //          clearInterval(setIntervalId);
-    //      }
-    //  }, 1);
+    var setIntervalId1 = setInterval(function(){
+        counter1+=0.111;
+        socialCounters[1].textContent = counter1.toString().substring(0, counter1.toString().indexOf('.') + 4).replace('.',',');
+        if(counter1 > span1){
+            clearInterval(setIntervalId1);
+            socialCounters[1].textContent = span1.toString().replace('.',',');
+        }
+    }, span1IntervalDelay);
+
+    var setIntervalId2 = setInterval(function(){
+        counter2+=0.111;
+        socialCounters[2].textContent = counter2.toString().substring(0, counter2.toString().indexOf('.') + 4).replace('.',',');
+        if(counter2 > span2){
+            clearInterval(setIntervalId2);
+            //wyliczenia nie sa celne co do 0.001 wiec pod koniec animacji i tak podstawiamy okreslona w htmlu wartosc
+            socialCounters[2].textContent = span2.toString().replace('.',',');
+        }
+    }, span2IntervalDelay);
 
 
 });
